94120b98287babf576ddbbe7cf1f38614e01bc5c
Brian Summers <bsummerscon@merkleinc.com>
2017-10-31 12:29:22 -0600

    Added material ui styles

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const InputButton = ({value, inputButtonClick}) => {
-    return <div className="calc-button" onClick={() => inputButtonClick(value)}>{value}</div>;
+    return <RaisedButton label={value} primary={true} onClick={() => inputButtonClick(value)} />;
 }
 
 const OperatorButton = ({value, operatorButtonClick}) => {
-    return <div className="calc-button" onClick={() => operatorButtonClick(value)}>{value}</div>;
+    return <RaisedButton  label={value} secondary={true} onClick={() => operatorButtonClick(value)} />;
 }
 
 const ClearButton = (props) => {
-    return <div className="calc-button__clear" onClick={() => props.reset()}>Clear</div>
+    return <RaisedButton label='Clear' onClick={props.reset}/>
 }
 
 class Calculator extends Component {
     constructor() {
+        super();
         this.state = {
             operator: '',
             input: '',
@@ -24,12 +26,14 @@ class Calculator extends Component {
     }
 
     inputClick = (value) => {
+        console.log('fired input click');
         this.setState({input: this.state.input+''+value, calculations: [this.state.input+''+value], lastClicked: 'input'});
     }
 
     operatorClick = (value) => {
+        console.log('fired operator click');
         if (this.state.lastClicked === 'input') {
-            
+
         }
     }
 
@@ -47,22 +51,22 @@ class Calculator extends Component {
             <article>
                 <div className="calc-value">{this.state.value}</div>
                 <ClearButton reset={this.resetCalc}/>
-                <OperatorButton value={'/'} operatorButtonClick={this.operatorClick('/')}/>
+                <OperatorButton value={'/'} operatorButtonClick={this.operatorClick}/>
                 <br/>
                 <InputButton value={1} inputButtonClick={this.inputClick}/>
                 <InputButton value={2} inputButtonClick={this.inputClick}/>
                 <InputButton value={3} inputButtonClick={this.inputClick}/>
-                <OperatorButton value={'*'} operatorButtonClick={this.operatorClick('*')}/>
+                <OperatorButton value={'*'} operatorButtonClick={this.operatorClick}/>
                 <br/>
                 <InputButton value={4} inputButtonClick={this.inputClick}/>
                 <InputButton value={5} inputButtonClick={this.inputClick}/>
                 <InputButton value={6} inputButtonClick={this.inputClick}/>
-                <OperatorButton value={'+'} operatorButtonClick={this.operatorClick('+')}/>
+                <OperatorButton value={'+'} operatorButtonClick={this.operatorClick}/>
                 <br/>
                 <InputButton value={7} inputButtonClick={this.inputClick}/>
                 <InputButton value={8} inputButtonClick={this.inputClick}/>
                 <InputButton value={9} inputButtonClick={this.inputClick}/>
-                <OperatorButton value={'-'} operatorButtonClick={this.operatorClick('-')}/>
+                <OperatorButton value={'-'} operatorButtonClick={this.operatorClick}/>
             </article>
         );
     }
